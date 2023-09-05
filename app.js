54940f63edce6fd68f66eb50b054d314939e9fa6
Adarsh <adarshsingh2k@gmail.com>
2023-09-05 18:39:49 +0530

    Lec 3 done

--- app.js
+++ app.js
@@ -5,7 +5,41 @@ import  ReactDOM  from "react-dom/client"
 
 // BABEL transpiles to React.createElement => ReactElement - which is a JS Object =>Rendered as HTML Element
 
-const heading=(<h1 className="heading">We Are up using JSX </h1>);
+// THis Is React Element
+const heading=(
+    <h1 className="heading">
+        We Are up using JSX 
+    </h1>
+);
+
+
+// React Functional Component
+
+/*
+    can be also written as simple arrow function
+    const Comp=()=>{
+        return <h1> Pass this arrow Func</h1>
+    }
+*/
+
+const Title=()=>(
+    <h1 className="heading">
+        We Are up using JSX 
+    </h1>
+);
+
+// the below syntax is called component composition
+const HeadingComponent= ()=>(
+    <div id="container">
+        {/* {console.log(5+'5')} -> this is correct we cna writ js using {} */}
+        {heading}
+    <Title/>
+
+        <h1 className="heading">Functional Component 🚀</h1>
+
+    </div>
+)
 
 const root= ReactDOM.createRoot(document.getElementById('root'));
-root.render(heading)
+// root.render(heading) --> we render React element  Like this
+root.render(<HeadingComponent/>) // We render React component like this
